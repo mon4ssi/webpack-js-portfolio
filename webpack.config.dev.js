3d91e7cf633f146d627680c3e802464a210a0192
Monassi Carrasco <mon4ssi@outlook.com>
2022-09-20 12:37:20 -0500

    [ADD] basic website using webpack

---
+++ webpack.config.dev.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const DotEnv = require('dotenv-webpack')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
+    },
+    mode: 'development',
+    watch: true,
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/fonts/[hash][ext]",
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets/images"),
+                    to: "assets/images",
+                }
+            ]
+        }),
+        new DotEnv(),
+    ],
+}
